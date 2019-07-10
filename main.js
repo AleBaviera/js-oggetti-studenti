32c325208a6d29bfddff0b5d322aa964aed46f87
alebaviera <baviera.alessandra@gmailcom>
2019-07-10 15:28:06 +0200

    nome e cognome

--- main.js
+++ main.js
@@ -32,14 +32,36 @@ for(var key in studente1){
 
 
 
-// - Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+// - Creare un array di oggetti di studenti. Ciclare su tutti gli studenti
+// e stampare per ognuno nome e cognome
 
 
+var studente2 = {
+  'nome': 'tutto',
+  'cognome': 'enulla',
+  'eta': 25
+}
 
+var studente3 = {
+  'nome': 'aciascuno',
+  'cognome': 'ilsuo',
+  'eta': 26
+}
 
+var studente4 = {
+  'nome': 'chinonrisica',
+  'cognome': 'nonrosica',
+  'eta': 27
+}
 
+var classe = [studente1, studente2, studente3, studente4];
 
+for (var i = 0; i < classe.length; i++) {
 
+    console.log(classe[i].nome);
+    console.log(classe[i].cognome);
+  
+}
 
 // - Dare la possibilità all’utente attraverso 3 prompt di
 // aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
